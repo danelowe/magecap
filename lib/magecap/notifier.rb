1b6819cfc9b6b235b027aa1617de333d32e07c71
Dane <dane@avidonline.co.nz>
2014-05-26 13:51:22 +1200

    notifier is working, needs major refactor though

--- lib/magecap/notifier.rb
+++ lib/magecap/notifier.rb
@@ -1,34 +1,69 @@
-require 'capistrano/notifier/mail'
-class Capistrano::Notifier::AvidMail < Capistrano::Notifier::Mail
+require 'pry'
+require 'action_mailer'
+class Magecap::Notifier
+  # @todo: split this into a base notifier class an mail notifier class based on the old-school capistrano-notifier
   def perform
+    options =  Capistrano::Configuration.fetch :notifier_mail_options
     notifier =  ActionMailer::Base
-    notifier.smtp_settings = smtp_settings
+    notifier.smtp_settings = options[:smtp_settings]
     notifier.mail({
         :body => html,
         :delivery_method => notifier.delivery_method,
-        :from => from,
+        :from => options[:from],
         :subject => subject,
-        :to => to,
+        :to => options[:to],
         :content_type => 'text/html'
     }).deliver
   end
 
   def body
     <<-BODY.gsub(/^ {6}/, '')
-#{user_name} deployed  #{application.titleize} branch #{branch} to stage #{stage}<br/>
-      #{now.strftime("%m/%d/%Y")} #{now.strftime("%I:%M %p %Z")}
+#{user_name} deployed  #{Capistrano::Configuration.fetch(:application).titleize} branch #{Capistrano::Configuration.fetch(:branch)} to stage #{Capistrano::Configuration.fetch(:stage)}<br/>
+      #{Time.now.strftime("%m/%d/%Y")} #{Time.now.strftime("%I:%M %p %Z")}
 
       <p><strong>#{git_range}</strong>
       #{git_log}</p>
     BODY
   end
 
+  def git_log
+    return unless git_range
+
+    `git log #{git_range} --no-merges --format=format:"%h %s (%an)"`
+  end
+
+  def git_previous_revision
+    Capistrano::Configuration.fetch(:previous_revision)
+  end
+
+  def git_current_revision
+    Capistrano::Configuration.fetch(:current_revision)
+  end
+
+  def git_range
+    return unless git_previous_revision && git_current_revision
+
+    "#{git_previous_revision}..#{git_current_revision}"
+  end
+
+  def subject
+    "#{Capistrano::Configuration.fetch(:application).titleize} branch #{Capistrano::Configuration.fetch(:branch)} deployed to #{Capistrano::Configuration.fetch(:stage)}"
+  end
+
   def github_commit_prefix
     "#{github_prefix}/commits"
   end
 
+  def github_compare_prefix
+    "#{github_prefix}/compare"
+  end
+
   def github_prefix
     "https://bitbucket.org/#{github}"
+  end
+
+  def github
+    Capistrano::Configuration.fetch(:notifier_mail_options)[:github]
   end
 
   def html
@@ -38,13 +73,18 @@ class Capistrano::Notifier::AvidMail < Capistrano::Notifier::Mail
         /^([0-9a-f]{7})/, "<br/><a href=\"#{github_commit_prefix}/\\0\">\\0</a>"
     )
   end
+
+  def user_name
+    user = ENV['DEPLOYER']
+    user = `git config --get user.name`.strip if user.nil?
+  end
 end
 namespace :deploy do
   namespace :notify do
     desc 'Send a deployment notification via email.'
     task :mail do
-      Capistrano::Notifier::AvidMail.new(configuration).perform
-      if configuration.notifier_mail_options[:method] == :test
+      Magecap::Notifier.new.perform
+      if Capistrano::Configuration.fetch(:notifier_mail_options)[:method] == :test
         puts ActionMailer::Base.deliveries
       end
     end
